a151b56fc015ead3e20a36e5939541052a2e31ed
Mann Patel <mann.patel.1207@gmail.com>
2026-05-04 23:54:31 +0530

    feat: replace tsconfig-paths with module-alias for path resolution and update route imports

--- src/routes/geocode.route.ts
+++ src/routes/geocode.route.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { auth } from "../middlewares/auth.middleware";
 import { GeocodeController } from "../controllers/geocode.controller";
 
 const router = Router();
